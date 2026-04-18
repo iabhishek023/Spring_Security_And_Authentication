b1fbfcc26c7797fcff855e38e61bb964691dddf8
Abhishek <abhishek.puri@lendingplate.com>
2026-04-18 14:38:37 +0530

    the setup for security and jwt

---
+++ spring-sec-demo/src/main/java/com/telusko/springsecdemo/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.telusko.springsecdemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.telusko.springsecdemo.model.User;
+import com.telusko.springsecdemo.service.UserService;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserService service;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+	
+	@PostMapping("register")
+	public User register(@RequestBody User user) {
+	  return service.saveUser(user);
+	}
+
+    @PostMapping("login")
+    public String login(@RequestBody User user){
+        Authentication authentication=authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
+
+        if (authentication.isAuthenticated())
+            return "Success";
+        else
+            return "Login failed";
+
+    }
+}
